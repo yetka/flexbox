838d1baf7c4b75af6f27507f2d52612dc82eef35
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2018-08-08 12:49:02 -0700

    create project to practise flexbox

---
+++ test.js
@@ -0,0 +1,85 @@
+// var name = "Gosia";
+// name = "Chrystelle"; 
+// console.log(name);
+// 
+// var lastName = "Haniszewska";
+// lastName = "Djuego";
+// console.log(lastName);
+// 
+// const boilingTemperature = 100;
+// 
+// var number = 21;
+// console.log(number);
+// var wordNumber = "21";
+// console.log(wordNumber);
+// 
+// var user_name = prompt("What is your name", "");
+// 
+// var stringData =  "Gosia";
+// var numberData = 36.21;
+// var boolianData =  true;
+// var test = "43";
+// 
+// document.write("my name is" + name + " and I say hello");
+// 
+// document.write("my name is");
+// document.write(name);
+// document.write("and I say hello");
+// 
+// alert("hi guys");
+// 
+// function testing_function(number1,number2) {
+//   var sum = number1 + number2;
+//   return sum;
+// }
+// 
+// var function_output = testing_function(3,4);
+// document.write("the result of adding two numbers is" + function_output);
+// 
+// document.write(testing_function(3,4));
+// 
+// var ages = testing_function(36,29);
+// document.write("our ages" + ages);
+// 
+// function full_name(first_name, last_name) {
+//   var both_names = first_name + last_name;
+//   return both_names;
+// }
+// 
+// var definition = full_name("Gosia ", " Djuego");
+// document.write("</br>");
+// document.write(definition);
+// 
+// function greeting() {
+//   var hello_message = "Hello!";
+//   return hello_message;
+// }
+// 
+// document.write(greeting());
+// 
+// var hello_greets = greeting();
+// document.write(hello_greets);
+
+
+// if (1>0) {
+//   alert("true")
+// };
+// 
+// var name = "Gosia"
+// 
+// if (name == "gosia") {
+//   alert("false")
+// } else {
+//   alert("true")
+// }
+// 
+// var number1 = 43;
+// var number2 = 23;
+// 
+// if (number1 < number2) {
+//   alert("I was right!!!!")
+// } else if (number1 == number2) {
+//   alert("ops")
+// } else {
+//   alert("try again")
+// }
